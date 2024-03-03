cb469db03ad630e0ceb8b077ec442d03c45a30cc
Rafał Ochotny <rafal.ochotny95@gmail.com>
2024-03-03 14:02:40 +0100

    added monaco editor

--- src/WebUi/app/page.tsx
+++ src/WebUi/app/page.tsx
@@ -1,4 +1,6 @@
-﻿export const runtime = "edge";
+﻿import TextEditor from "@/components/Editor/TextEditor";
+
+export const runtime = "edge";
 
 import {getTokens} from "@/api/getTokens";
 export default async function Page(){
@@ -8,7 +10,7 @@ export default async function Page(){
     return (
     <div className={"pt-4 flex items-center justify-between w-full"}>
         <div className={"w-full"}>Diagram</div>
-        <div className={"w-full"}>Editor</div>
+        <div className={"w-full"}><TextEditor/></div>
         <div>{JSON.stringify(newVar)}</div>
     </div>)
 }
---
+++ src/WebUi/components/Editor/TextEditor.tsx
@@ -0,0 +1,116 @@
+﻿"use client";
+import {useEffect, useState} from "react";
+import Editor, {useMonaco} from '@monaco-editor/react';
+export default function TextEditor(){
+    
+    const [value, setValue] = useState("sln SolutionName {\n" +
+        "   folder DummyFolder {\n" +
+        "       csharp ProjectName {\n" +
+        "\n" +
+        "       }\n" +
+        "\n" +
+        "       csharp SecondProject {\n" +
+        "\n" +
+        "        }\n" +
+        "   }\n" +
+        "   csharp ExampleProject {\n" +
+        "\n" +
+        "   }\n" +
+        "   \n" +
+        "   folder NewFolder {\n" +
+        "   \n" +
+        "   } \n" +
+        "} ")
+    
+    const monaco = useMonaco();
+    
+    monaco?.languages.register({id: 'slg'})
+
+    monaco?.editor.defineTheme('slg', {
+        base: 'vs',
+        inherit: true,
+        rules: [
+            {
+                token: 'keyword',
+                foreground: '#A020F0'
+            },
+            {
+                token: 'type.identifier',
+                foreground: "#ffffff"
+            },
+            {
+                token: '@brackets',
+                foreground: '#ffffff'
+            },
+            
+        ],
+        colors: {
+            'editor.background': '#0F666405',
+            'editor.foreground': '#aaaabc',
+            
+        }
+    })
+    
+    monaco?.languages.setMonarchTokensProvider('slg', {
+        keywords: [ 'sln', 'folder', 'csharp' ],
+        typeKeywords: ['sln', 'folder', 'folder'],
+        symbols:  /[=><!~?:&|+\-*\/\^%]+/,
+        escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+        tokenizer: {
+            root: [
+                [/[a-z_$][\w$]*/, { cases: { '@typeKeywords': 'keyword',
+                        '@keywords': 'keyword',
+                        '@default': 'identifier' } }],
+                [/[A-Z][\w\$]*/, 'type.identifier' ],
+
+                { include: '@whitespace' },
+
+                [/[{}()\[\]]/, '@brackets'],
+                [/[<>](?!@symbols)/, '@brackets'],
+
+                [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
+                [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
+
+                // characters
+                [/'[^\\']'/, 'string'],
+                [/(')(@escapes)(')/, ['string','string.escape','string']],
+                [/'/, 'string.invalid']
+                
+            ],
+
+            string: [
+                [/[^\\]+/,  'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./,      'string.escape.invalid'],
+            ],
+            
+            whitespace: [
+                [/[ \t\r\n]+/, 'white']
+            ],
+           
+        }
+    })
+
+    
+        useEffect(() => {
+          
+        }, []);
+
+
+        return (
+            <div id={"container"} style={{height: '100%'}}>
+                <Editor
+                    theme={"slg"}
+                    height={"70vh"} language={"slg"} value={value} onChange={(e) => {
+                    const val = e ?? ""; 
+                    setValue(val);
+                }} 
+                
+                        options={{
+                            fontSize: 18
+                        }}
+                />
+            </div>
+        )
+    
+}
