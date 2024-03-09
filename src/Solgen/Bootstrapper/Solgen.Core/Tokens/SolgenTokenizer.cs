bb5512ce9666f6e33d3c16687e3fdfd62739dc5a
rafoch <rafal.ochotny95@gmail.com>
2024-03-09 21:36:23 +0100

    Changed implementation. Added exception when tokenizer returns more than two possible results

--- src/Solgen/Bootstrapper/Solgen.Core/Tokens/SolgenTokenizer.cs
+++ src/Solgen/Bootstrapper/Solgen.Core/Tokens/SolgenTokenizer.cs
@@ -12,22 +12,36 @@ public class SolgenTokenizer
 
     public IEnumerable<TokenResult> GetTokens(string uml)
     {
-        var umlWords = uml.Replace(System.Environment.NewLine, String.Empty).Split(' ').Where(x => x != String.Empty);
+        // Replacing all unnecessary characters from string
+        var umlWords = uml
+            .Replace(System.Environment.NewLine, String.Empty).Split(' ')
+            .Where(x => x != String.Empty);
+        
+        // setting depth level based on language open and closing brackets
         var depth = 1;
         var tokenIndex = 0;
         var tokens = new List<TokenResult>();
+        
+        //TODO change to regex
         foreach (var word in umlWords)
         {
             foreach (var tokenizer in _tokenizers)
             {
                 var result = tokenizer.GetTokens(word, ref depth, ref tokenIndex);
 
+                //When tokenizer doesnt return anything it means the word is unknow, should be resolved as error.
                 if (result.Any(x => x.Token == null))
                 {
                     continue;
                 }
                 
                 tokens.AddRange(result);
+                
+                // there always is only ONE result from language tokenizer. When there are more than one then the code should break
+                if (result.Count() > 1)
+                {
+                    throw new Exception("Tokenizer occured an error, there are more than one token based on provided input: {Error}");
+                }
                 
                 var tokenResult = result.First();
                 if (tokenResult.Token is { HasIdentifier: true })
@@ -43,6 +57,7 @@ public class SolgenTokenizer
         return tokens.Where(x => x.Token != null);
     }
 
+    //TODO that's doesnt do anthing right now. It probably need a rewrite
     public IList<Token> GetAllAvailableTokens()
     {
         var tokens = _tokenizers.SelectMany(x => x.GetAvailableTokens()).ToList();
