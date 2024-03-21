953dcc2709f94ea012ce9f637c6623b47f415f2c
rafoch <rafal.ochotny95@gmail.com>
2024-03-21 18:45:03 +0100

    changed about

--- src/WebUi/app/about/Page.tsx
+++ src/WebUi/app/about/Page.tsx
@@ -1,7 +1,7 @@
 ﻿export default function Page(){
     return (
     <div className="w-full flex items-center flex-col justify-start gap-4">
-        <section>About me as a developer</section>
+        <section>About me as a developer. Hello, my name is <b>Rafał</b></section>
         <section>About the problem </section>
     </div>
     )
