7df810804acedc5a0884aa4f42fd9bd552c2060a
Rafał Ochotny <rafal.ochotny95@gmail.com>
2024-03-28 20:15:50 +0100

    changeed about page

--- src/WebUi/app/about/Page.tsx
+++ src/WebUi/app/about/Page.tsx
@@ -1,8 +1,23 @@
 ﻿export default function Page(){
+    const dateNow = new Date();
+    const beginDate = new Date(2019,1, 1);
+    const milliDiff: number = dateNow.getTime()
+    - beginDate.getTime();
+    const totalSeconds = Math.floor(milliDiff / 1000);
+ 
+    // Total number of minutes in the difference
+    const totalMinutes = Math.floor(totalSeconds / 60);
+     
+    const totalHours = Math.floor(totalMinutes / 60);
+
+    const totalDays = Math.floor(totalHours / 24)
+
+    const totalYears = Math.floor(totalDays / 365)
     return (
-    <div className="w-full flex items-center flex-col justify-start gap-4">
+    <div className="w-full container flex items-center flex-col justify-start gap-4">
         <section>About me as a developer. Hello, my name is <b>Rafał</b></section>
-        <section>About the problem </section>
+        <section>For now I've got {totalYears} years of experience in .NET</section>
+        <section>Contact</section>
     </div>
     )
 }
