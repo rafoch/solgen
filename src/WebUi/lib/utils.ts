d05eb2b0ad574f8b5d0f0a28feda8f0459677d47
Rafał Ochotny <rafal.ochotny95@gmail.com>
2024-04-07 19:27:12 +0200

    added utils functions

--- src/WebUi/lib/utils.ts
+++ src/WebUi/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertDateToString(date: Date){
+  return date.toLocaleDateString();
+}
+
+export function toDate(dateString: string){
+  return new Date();
+}
+
+export function getModificationDateString(date: Date){
+  return "Yesterday";
+}
